ac39a33fd05c396dfdc9cf61c0694066425a289d
Mihai-Andrei Ciupitu <ciupitu.mihai7@gmail.com>
2024-02-05 20:51:46 +0000

    added dark theme and fixed icon

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.info("Ready");
+// Dark Theme
+const icon = document.getElementById("icon");
+const imgdark = document.querySelector(".image");
+const logodark = document.querySelector(".logo");
+const logo2dark = document.querySelector(".logo2");
+const logo3dark = document.querySelector(".logo3");
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  imgdark.classList.toggle("image-dark");
+  logodark.classList.toggle("logo-dark");
+  logo2dark.classList.toggle("logo-dark");
+  logo3dark.classList.toggle("logo-dark");
+
+  if (document.body.classList.contains("dark-theme")) icon.src = "sun.png";
+  else icon.src = "moon.png";
+};
